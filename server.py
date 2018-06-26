4c62e8c50909009abacc9afc4d1a1cf4e407aaad
Jonathan Drake <jdrake@narrativescience.com>
2018-06-26 00:22:38 -0500

    runner

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
+"""
+Advanced hyper text transport protocol-based application programming
+interface
+"""
+import logging
+import os
+
 from aiohttp import web
 
 
+logging.basicConfig(level=logging.INFO)
+
+
 async def hello(request):
-    return web.Response(text="Hello, world")
+    return web.Response(text=f"Hello, {os.environ.get('GUEST', 'World')}")
 
 
 app = web.Application()
